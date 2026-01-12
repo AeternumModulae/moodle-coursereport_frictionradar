265b8e0a6776cf7ab6d286ff511e5fd862405423
Jan Svoboda <jan.svoboda@bitTRA.DE>
2026-01-12 12:55:48 +0100

    Add copyright comments

--- classes/task/queue_cache_warmers.php
+++ classes/task/queue_cache_warmers.php
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2026 Jan Svoboda <jan.svoboda@bittra.de>
+ * Project: Aeternum Modulae – https://aeternummodulae.com
+ *
+ * This file is part of the Aeternum Modulae Moodle plugin "Friction Radar".
+ *
+ * Licensed under the GNU General Public License v3.0 or later.
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 <?php
 namespace tool_frictionradar\task;
 
