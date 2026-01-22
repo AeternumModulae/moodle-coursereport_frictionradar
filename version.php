285ceaccebfa008d9f5dae5d17af6d4846c23b63
Jan Svoboda <jan.svoboda@bitTRA.DE>
2026-01-22 10:00:40 +0100

    Increase version

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'coursereport_frictionradar';
-$plugin->version   = 2026012201; // YYYYMMDDXX.
+$plugin->version   = 2026012202; // YYYYMMDDXX.
 $plugin->requires  = 2024100700; // Moodle 4.5 (approx build version).
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '0.3.0';
+$plugin->release   = '0.3.1';
